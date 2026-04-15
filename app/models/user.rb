c2968761972579dd611a04ebbd9e08dc23b15f71
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-15 16:54:39 +0900

    バリデーション(正規表現含む)の設定、ヘッダーのログイン状態の有無での表示の切り替えの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, :birthday_on, presence: true
+  validates :password, format: {with:/(?=.*\d)(?=.*[a-z])/i }, presence: true
+  validates :last_name, :first_name, format: {without: /[a-z\d]/i },  presence: true
+  validates :first_name_kana, :last_name_kana, format: {with: /\A[ア-ヵー]+\z/}, presence: true
+
 end
