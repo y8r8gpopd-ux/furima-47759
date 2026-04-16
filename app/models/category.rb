013b95cf388e7df5ccf108de0b1ea9240a7e7059
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-16 21:04:14 +0900

    ActiveHashモデルの誤った記述を修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveHash::Base
 
-  self.date = [
+  self.data = [
 
     { id: 1, name: "---"},
     { id: 2, name: "メンズ"},
--- app/models/shipping_fee.rb
+++ app/models/shipping_fee.rb
@@ -1,6 +1,6 @@
 class ShippingFee < ActiveHash::Base
 
-  self.data[
+  self.data = [
 
     {id: 1, name:"---"},
     {id: 2, name:"着払い(購入者負担)"},
