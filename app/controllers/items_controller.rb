729becfb3cffb3d7f6aff9490b427e3e6641aa02
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-17 11:48:21 +0900

    itemsコントローラーの設定　(index､new、create)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,37 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
   end
 
   def new
     @item = Item.new
   end
   
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render action: :new
+    end
+  end
+
+
+
+
+
+  private
+    def item_params
+      params.require(:item).permit(:name,
+                                   :price, 
+                                   :description, 
+                                   :category_id,
+                                   :condition_id, 
+                                   :shipping_fee_id, 
+                                   :prefecture_id,
+                                   :shipping_time_id,
+                                   :birthday_on,
+                                   :image).merge(user_id: current_user.id)
+    end
+
 end
