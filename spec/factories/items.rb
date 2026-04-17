064e5a6410a4c00cd6c9bef7082b267522f805ad
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-17 18:44:46 +0900

    itemuのFactoryBotの設定

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+    name {Faker::Commerce.product_name}
+    price {1000}
+    description {Faker::Lorem.sentence}
+    category_id {2}
+    condition_id {2}
+    shipping_fee_id {2}
+    prefecture_id {2}
+    shipping_time_id {2}
+
+    association :user
   end
+
+  after(:build) do |item|
+    item.image.attach(io: File.open ('app/assets/images/google-play.png'), filename: 'test.png')
+  end
+  
 end
