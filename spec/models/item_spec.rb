160891bf7960ebaee67aee53aed596ebf78c24ad
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-18 12:40:32 +0900

    テストコード修正、出品機能の完成

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -85,7 +85,6 @@ RSpec.describe Item, type: :model do
       it "価格(price)が9,999,999円までしかつけられない" do
         @item.price = 10000000
         @item.valid?
-        binding.pry
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
       end
 
