c53561b21664e7801e64146f0fd79a165ced327f
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-18 18:05:23 +0900

    Revert "商品一覧表示機能"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: :index
 
   def index
-    @items = Item.order("created_at desc")
+    # @items = Item.all
   end
 
   def new
