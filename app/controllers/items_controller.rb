9d32373c086a8e8649779d948106401c2cdbac27
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-20 17:24:36 +0900

    商品編集ページの編集、編集完了時の画面遷移の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
 
   def update
     if @item.update(item_params)
-      redirect_to :show
+      redirect_to @item
     else
       render :edit, status: :unprocessable_entity
     end
