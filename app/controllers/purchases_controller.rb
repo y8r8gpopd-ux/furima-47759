6bc3152ef78e71ec94dcd5be6a2d15cd5bc399f6
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-23 15:55:15 +0900

    formオブジェクトのストロングパラメータ設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,6 +4,20 @@ class PurchasesController < ApplicationController
   end
 
   def create
-    
+    @purchase_shipping_address = PurchaseShippingAddress.new(p_s_a_params)
+    @purchase_shipping_address.save
   end
+
+
+  private
+    def p_s_a_params
+      params.require(:purchase_shipping_address).permit(:post_code,
+                                                        :prefecture_id,
+                                                        :address,
+                                                        :house_number,
+                                                        :building,
+                                                        :tel).merge(uer_id: current_user.id,
+                                                                    item_id: @item.id)
+  end
+
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -2,9 +2,16 @@ class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :address, :house_number, :building, :tel
 
-  with_option presence: true do
+  with_options presence: true do
     validates :user_id, :item_id, :address, :house_number
     validates :post_code, format: { with: /\A\d{3}-\d{4}\z/ } 
     validates :tel, format: { with: /\A\d{10}|\d{11}\z/ }
     validates :prefecture_id, numericality: {other_than: 1}
+  end
+
+  def save
+    purchase = Purchase.create(:user_id, :item_id)
+    shipping_address.create(:post_code, :prefecture_id, :address, :house_number, :building, :tel, :purchase_id)
+  end
+
 end
