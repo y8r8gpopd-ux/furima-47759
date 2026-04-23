bfb467a91ed57c90c37708039de0b11af0e11ba3
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-23 12:35:08 +0900

    ルーティングの見直し、購入ページの配置

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "items#index"
   resources :items do
-    resources :purchases, only: :create
+    resources :purchases, only: [:index, :create]
   end
 end
