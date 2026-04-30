c307e32d68bcf2733f3b190c18b34e2065fd6a11
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-04-30 15:47:00 +0900

    自動デプロイの設定　capistranoの導入

---
+++ config/deploy.rb
@@ -0,0 +1,26 @@
+lock '3.20.0'
+
+set :application, 'furima-47759'
+
+set :repo_url,  'git@github.com:y8r8gpopd-ux/furima-47759.git'
+set :branch, 'main'
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '3.2.0' 
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/my-key-pair.pem'] 
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
