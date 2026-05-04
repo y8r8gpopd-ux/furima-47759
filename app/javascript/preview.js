64f5d9f08040a6fb14f697c40d05b676f21cd3d2
y8r8gpopd-ux <y8r8g.popd@gmail.com>
2026-05-04 13:29:50 +0900

    複数画像の投稿機能、画像の選択しなおし、削除ボタンの実装

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -3,27 +3,80 @@ const preview = function(){
   const sellBtn = document.querySelector(".sell-btn");
   if(!sellBtn) return;
 
-  const preview = document.getElementById("previews");
-  const imageForm = document.getElementById("item-image");
+  const previewArea = document.getElementById("previews");
 
-
-  imageForm.addEventListener("change", function(e){
-    // プレビュー交換する場合前のを消す記述↓↓
-    const alreadyPreview = document.querySelector(".preview-img");
-    if(alreadyPreview){
-      alreadyPreview.remove();
-    }
-    // プレビュー交換する場合前のを消す記述↑↑
-
-    const imageFile = e.target.files[0];
-    const blob = URL.createObjectURL(imageFile);
-
-    // 後でラッパーいるか？
+    //プレビュー生成関数 
+  const buildPreview = (dataIndex, blob) => {
     const previewImg = document.createElement("img");
     previewImg.setAttribute("class", "preview-img");
+    previewImg.setAttribute("data-index", dataIndex);
     previewImg.src = blob;
-    preview.appendChild(previewImg);
-  })
+
+    // 削除ボタンも作成
+    const deleteBtn = document.createElement("div");
+    deleteBtn.setAttribute("class", "img-delete-btn");
+    deleteBtn.setAttribute("data-index", dataIndex);
+    deleteBtn.textContent = "削除";
+    deleteBtn.addEventListener("click", () => deleteImg(dataIndex));
+
+    previewArea.appendChild(previewImg);
+    previewArea.appendChild(deleteBtn);
+  }
+
+    // 二枚目以降の投稿ボタン生成関数
+  const buildNewForm = () => {
+    const imgFormArea = document.querySelector(".click-upload");
+    const newImgForm = document.createElement("input");
+    newImgForm.setAttribute("type", "file");
+    newImgForm.setAttribute("name", "item[images][]");
+
+    // 二枚目以降のナンバリング
+    const lastImgIndex = document.querySelector('input[type="file"][name="item[images][]"]:last-child');
+    const nextImgIndex = Number(lastImgIndex.getAttribute("data-index")) + 1;
+    newImgForm.setAttribute("data-index", nextImgIndex);
+
+    newImgForm.addEventListener("change", changeFile);
+    imgFormArea.appendChild(newImgForm);
+  }
+
+  // ファイルが空の時フォームを削除する関数
+  const deleteImg = (dataIndex) => {
+    const deletePreview = document.querySelector(`.preview-img[data-index="${dataIndex}"]`);
+    deletePreview.remove();
+
+    const deleteImgForm = document.querySelector(`input[type="file"][data-index="${dataIndex}"]`);
+    deleteImgForm.remove();
+
+    const deleteDeleteBtn = document.querySelector(`.img-delete-btn[data-index="${dataIndex}"]`);
+    deleteDeleteBtn.remove();
+  }
+
+
+    // 投稿ボタンが触れられた時の関数
+  const changeFile = (e) => {
+    const dataIndex = e.target.getAttribute('data-index');
+    const imgFile = e.target.files[0];
+
+    // 画像が何も選択されなかったらdeleteImgを呼び出す
+    if (!imgFile){
+      deleteImg(dataIndex);
+      return ;
+    }
+
+    const blob = URL.createObjectURL(imgFile);
+
+    const alreadyPreview = document.querySelector(`.preview-img[data-index="${dataIndex}"]`);
+    if(alreadyPreview){
+      alreadyPreview.src = blob;
+      return ;
+    };
+
+    buildPreview(dataIndex, blob);
+    buildNewForm();
+  }
+
+  const imgForm = document.getElementById("item-image");
+  imgForm.addEventListener("change", changeFile);
 };
 
 
